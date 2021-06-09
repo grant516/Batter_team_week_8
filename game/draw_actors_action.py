147319ec5ac9ad24fafc84e86ace4ac5f43180b7
Grant Shirley <grantmshirley516@gmail.com>
2021-06-09 13:42:52 -0600

    the repository

---
+++ game/draw_actors_action.py
@@ -0,0 +1,31 @@
+from game.action import Action
+
+# TODO: Define the DrawActorsAction class here
+
+class DrawActorsAction(Action):
+    
+    def __init__(self, output_service):
+
+        self._output_service = output_service
+
+    def execute(self, cast):
+        """Executes the action using the given actors.
+
+        Args:
+            cast (dict): The game actors {key: tag, value: list}.
+        """
+        self._output_service.clear_screen() #clears the screen
+
+        """for group in cast.values():
+            for actor in group:
+                if not actor.get_velocity().is_zero():
+                    self._move_actor(actor)"""
+
+        self._output_service.draw_actors(cast["marquee"])
+        self._output_service.draw_actors(cast["robot"])
+        self._output_service.draw_actors(cast["artifact"])
+        
+
+        self._output_service.flush_buffer() #updates the new things on the screen
+
+        
